2fd6af607e012273814241f0183dc3a60e32756b
mikel evins <mikel@evins.net>
2021-11-07 11:04:47 -0600

    adding webvbiew API functions

--- clio-cffi.lisp
+++ clio-cffi.lisp
@@ -2,6 +2,7 @@
 
 (in-package #:clio-cffi)
 
+;;; ---------------------------------------------------------------------
 ;;; windows
 ;;; ---------------------------------------------------------------------
 
@@ -32,15 +33,55 @@
 #+win32
 (cffi:use-foreign-library webview-lib)
 
+;;; ---------------------------------------------------------------------
+;;; API definitions
+;;; ---------------------------------------------------------------------
+
+
 (cffi:defcfun (webview-create "webview_create" :library webview-lib) :pointer
   (debug :int)
   (window :pointer))
 
-#+nil (setf $wv (webview-create 1 (cffi:null-pointer)))
+(cffi:defcfun (webview-destroy "webview_destroy" :library webview-lib) :void
+  (webview :pointer))
 
+#+nil (setf $wv (webview-create 1 (cffi:null-pointer)))
+#+nil (webview-destroy $wv)
+
+(cffi:defcfun (webview-run "webview_run" :library webview-lib) :void
+  (webview :pointer))
+
+#+nil (setf $wv (webview-create 1 (cffi:null-pointer)))
+#+nil (webview-run $wv)
+#+nil (webview-destroy $wv)
+
+(cffi:defcfun (webview-terminate "webview_terminate" :library webview-lib) :void
+  (webview :pointer))
+
+;;; skip this one for now; it needs us to pass a function pointer, so it's inconvenient to implement
+;;; void webview_dispatch(webview_t w, void (*fn)(webview_t w, void *arg), void *arg);
+
+ ;; Returns a native window handle pointer. When using GTK backend the pointer
+ ;; is GtkWindow pointer, when using Cocoa backend the pointer is NSWindow
+ ;; pointer, when using Win32 backend the pointer is HWND pointer.
+(cffi:defcfun (webview-get-window "webview_get_window" :library webview-lib) :pointer
+  (webview :pointer))
+
+(cffi:defcfun (webview-set-title "webview_set_title" :library webview-lib) :pointer
+  (webview :pointer)
+  (title :string))
+
+#+nil (setf $wv (webview-create 1 (cffi:null-pointer)))
+#+nil (webview-set-title $wv "Hello!")
+#+nil (webview-destroy $wv)
+
+
+
+;;; ---------------------------------------------------------------------
 ;;; macos
 ;;; ---------------------------------------------------------------------
 
+;;; ---------------------------------------------------------------------
 ;;; linux
 ;;; ---------------------------------------------------------------------
 
